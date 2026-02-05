721059a532998ef92fa77aec5bc668ed713ac5bb
Abyss <abyss@users.noreply.github.com>
2026-02-05 14:58:31 -0600

    fix: Improved git config

--- util/git-config.sh
+++ util/git-config.sh
@@ -15,3 +15,26 @@ git config --global init.defaultBranch main
 # Don't automatically merge on diverged pull. I'll deal with those manually.
 printf "git config --global pull.ff only\n"
 git config --global pull.ff only
+
+# Git Aliases
+printf "Configuring git aliases:\n"
+printf "  - co → checkout\n"
+git config --global alias.co checkout
+
+printf "  - br → branch\n"
+git config --global alias.br branch
+
+printf "  - ci → commit\n"
+git config --global alias.ci commit
+
+printf "  - st → status\n"
+git config --global alias.st status
+
+printf "  - last → log -1 HEAD\n"
+git config --global alias.last 'log -1 HEAD'
+
+printf "  - unstage → reset HEAD --\n"
+git config --global alias.unstage 'reset HEAD --'
+
+printf "  - log-tree → a nicely formatted git log graph\n"
+git config --global alias.log-tree '!git log --graph --abbrev-commit --decorate --date=relative --format=format:"%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(bold black)[%an]%C(reset)%C(bold yellow)%d%C(reset)" --all'
